a7347c0c81444ddfccb16e65108be6e826969240
johnemersond <57560808+johnemersond@users.noreply.github.com>
2019-11-09 19:49:34 +0530

    Create fullsaycheesecommand.sh

---
+++ fullsaycheesecommand.sh
@@ -0,0 +1,10 @@
+pkg update
+pkg install php
+pkg install git
+pkg install openssh
+git clone https://github.com/thelinuxchoice/saycheese
+ls
+cd saycheese
+ls
+chmod +x saycheese.sh
+bash saycheese.sh
